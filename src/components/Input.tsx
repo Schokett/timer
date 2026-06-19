841073c83d1bb6af6341123a1ca4688fbf3bdea3
schokett <skrollanelenazimmermann@gmail.com>
2026-06-20 00:22:25 +0200

    forbidden minus number in input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,9 +13,15 @@ function Input({ startValue, setTimerValue, setStartValue, hasError }: Props) {
       {/* <p className="section-timer__title">Zeit festlegen:</p> */}
       <input
         type="number"
+        min={0}
         className={`section-timer__timer-input ${hasError ? "is-invalid" : ""}`}
         value={startValue === 0 ? "" : startValue}
         autoFocus
+        onKeyDown={(event) => {
+          if (event.key === "-" || event.key === "Subtract") {
+            event.preventDefault();
+          }
+        }}
         onChange={(event) => {
           const value = Number(event.target.value);
           setTimerValue(value);
